40d0fb35d6607d98a00731d97fe44a0a542b94ad
Jayanti Roy <98177314+JayantiRoy@users.noreply.github.com>
2024-07-02 21:33:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CountdownTimer from './components/countdownTimer/countdownTimer';
+
+const App = () => {
+  return (
+    <div className="App">
+      <CountdownTimer />
+    </div>
+  );
+};
+
+export default App;
